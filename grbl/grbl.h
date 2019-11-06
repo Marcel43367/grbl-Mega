9d3b905327c4decd9fc3acda4eb49b9f290c8397
Marcel43367 <50874321+Marcel43367@users.noreply.github.com>
2019-11-06 12:49:16 +0100

    Update grbl.h
    include arduino library for output handling

--- grbl/grbl.h
+++ grbl/grbl.h
@@ -61,6 +61,7 @@
 #include "stepper.h"
 #include "jog.h"
 #include "sleep.h"
+#include "Arduino.h"    
 
 // ---------------------------------------------------------------------------------------
 // COMPILE-TIME ERROR CHECKING OF DEFINE VALUES:
